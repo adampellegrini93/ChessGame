9139f6d7721071e0e26d1f23bf08517ac43c171c
Kdesyne <andrewkart93@gmail.com>
2018-09-24 23:12:59 -0400

    Added method for random move generation - initial

--- src/chessgame/ChessAI.java
+++ src/chessgame/ChessAI.java
@@ -1,4 +1,19 @@
 package chessgame;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
 public class ChessAI {
+
+    //Currently a placeholder for *random* move generation - for "Easy" difficulty(?)
+    //Need to know how legal moves are checked for in our application
+    static double newMove;
+
+    public static double calculateMove()
+    {
+        return newMove;
+    }
+
 }
