92467044c96d952a143db3a830cd6e937fb1171b
Kdesyne <andrewkart93@gmail.com>
2018-10-08 13:14:36 -0400

    Added to execute() in MiniMax.java, fixed error in max()

--- Netbean Chess Code/ChessProject/src/ai/MiniMax.java
+++ Netbean Chess Code/ChessProject/src/ai/MiniMax.java
@@ -30,9 +30,40 @@ public class MiniMax implements MoveAlgorithm{
     public Move execute(Board board, int depth){
         
         
+        final long startTime = System.currentTimeMillis();
         
+        Move bestMove = null;
         
-        return null;
+        int maxSeenValue = Integer.MIN_VALUE;
+        int minSeenValue = Integer.MAX_VALUE;
+        
+        int currentValue;
+        
+        System.out.println(board.currentPlayer() + " THINKING with depth = " + depth);
+        
+        int numMoves = board.currentPlayer().getLegalMoves().size();
+        
+        for(final Move move : board.currentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+            if(moveTransition.getMoveStatus().isDone()) {
+                //if current player is white, next move is a minimizing move, vice versa
+                currentValue = board.currentPlayer().getAlliance().isWhite() ?
+                        min(moveTransition.getTransitionBoard(), depth - 1) :
+                        max(moveTransition.getTransitionBoard(), depth - 1);
+                
+                if(board.currentPlayer().getAlliance().isWhite() && currentValue >= maxSeenValue) {
+                    maxSeenValue = currentValue;
+                    bestMove = move;
+                }
+                else if(board.currentPlayer().getAlliance().isBlack() && currentValue <= minSeenValue) {
+                    minSeenValue = currentValue;
+                    bestMove = move;
+                }
+            }
+        }
+        final long executionTime = System.currentTimeMillis() - startTime;
+        
+        return bestMove;
     }
     
     public int min (final Board board, final int depth){
@@ -65,7 +96,7 @@ public class MiniMax implements MoveAlgorithm{
         for(final Move move : board.currentPlayer().getLegalMoves()) {
             final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
             if(moveTransition.getMoveStatus().isDone()) {
-                final int currentValue = max(moveTransition.getTransitionBoard(), depth - 1);
+                final int currentValue = min(moveTransition.getTransitionBoard(), depth - 1);
                 if(currentValue >= maxSeenValue) {
                     maxSeenValue = currentValue;
                 }
